46b58faf04a8486dc35dfae4bf9bea78e59e081e
inlancersystem4 <inlancersystem4@gmail.com>
2024-11-21 12:57:15 +0530

    push

--- django/demo_app/models.py
+++ django/demo_app/models.py
@@ -12,3 +12,11 @@ class UserRole(models.Model):
 
     class Meta:
         db_table = 'userrole'
+
+
+class Module(models.Model):
+    module_id = models.AutoField(primary_key=True)
+    module_name = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = "module"
