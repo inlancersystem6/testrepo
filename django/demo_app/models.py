2c1fac1186105a1b94fd60a0b2c8c13dd758b4ec
inlancersystem6 <132996040+inlancersystem6@users.noreply.github.com>
2024-11-21 12:45:10 +0530

    commit

---
+++ django/demo_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class UserRole(models.Model):
+
+    role_id             =       models.AutoField(primary_key=True)
+    role_name           =       models.CharField(max_length=50)
+    role_status         =       models.BooleanField()
+    role_created_at     =       models.DateTimeField(auto_created=True)
+    role_is_delete      =       models.BooleanField(default=0)
+    role_updated_at     =       models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'userrole'
